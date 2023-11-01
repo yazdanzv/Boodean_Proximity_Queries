7d667f150c509255cc24f38d31b8daae78fe7cff
Yazdan ZandiyeVakili <yazdanzv.1378@gmail.com>
2023-11-01 22:20:02 +0330

    preprocessing section is complete

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,27 @@
 import os
 import re
+import copy
+import nltk
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 
 class PreProcess:
     def __init__(self, documents_directory: str = ".\\Documents"):
         self.directory = documents_directory
         self.docs = []
+        self.tokens = []
+        self.stemmed_tokens = []
+        self.lemmatized_tokens = []
         self.load_docs()
         self.start()
-        print(self.docs)
+
 
     def load_docs(self):
         # Loop through the files in the directory
@@ -28,23 +41,51 @@ class PreProcess:
         normalized_text = re.sub(r'[^\w\s]', '', text)
         return normalized_text
 
-    def stemming(self):
-        pass
-    pass
+    def tokenize(self):
+        for i in range(len(self.docs)):
+            self.tokens.append(word_tokenize(self.docs[i]))
 
     def stop_word_remover(self):
-        pass
+        stop_words = set(stopwords.words('english'))
+        tokens_without_stopwords = []
+        for i in range(len(self.tokens)):
+            tokens = [word for word in self.tokens[i] if word not in stop_words]
+            tokens_without_stopwords.append(copy.deepcopy(tokens))
+        self.tokens = copy.deepcopy(tokens_without_stopwords)
+
+    def stemming(self):
+        stemmer = PorterStemmer()
+        stemmed_tokens_list = []
+        for i in range(len(self.tokens)):
+            stemmed_tokens = [stemmer.stem(word) for word in self.tokens[i]]
+            stemmed_tokens_list.append(stemmed_tokens)
+        self.tokens = copy.deepcopy(stemmed_tokens_list)
 
     def lemmatization(self):
-        pass
-
-    def tokenizing(self):
-        pass
+        lemmatizer = WordNetLemmatizer()
+        lemmatized_words_list = []
+        for i in range(len(self.tokens)):
+            lemmatized_words = [lemmatizer.lemmatize(word, pos="v") for word in self.tokens[i]]
+            lemmatized_words_list.append(lemmatized_words)
+        self.tokens = copy.deepcopy(lemmatized_words_list)
 
     def start(self):
         for i in range(len(self.docs)):
             self.docs[i] = self.case_folding(self.docs[i])  # Handle Upper cases
             self.docs[i] = self.special_characters_remover(self.docs[i])  # Eliminate Special Characters
+        self.tokenize()
+        print(self.docs)
+        print(self.tokens)
+        print(len(self.tokens))
+        self.stop_word_remover()
+        print(self.tokens)
+        print(len(self.tokens))
+        self.stemming()
+        print(self.tokens)
+        print(len(self.tokens))
+        self.lemmatization()
+        print(self.tokens)
+        print(len(self.tokens))
 
 
 a = PreProcess()
