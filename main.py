508321877a0a3db6e41046da31c3c759a3e40a1a
Yazdan ZandiyeVakili <yazdanzv.1378@gmail.com>
2023-11-10 04:07:29 +0330

    final cleaning

--- main.py
+++ main.py
@@ -22,16 +22,24 @@ print(c.inverted_index)
 print("******************************************************")
 #  You can see all the queries that this IR system can handle and it can actually handle two ways of NOT query
 # Simple Queries
+print("Simple AND")
 q1 = Query('heart AND city', b.inverted_index_optimized, c.inverted_index_optimized, len(a.docs), a.terms, a.terms_edited)
+print("Simple OR")
 q2 = Query('heart OR city', b.inverted_index_optimized, c.inverted_index_optimized, len(a.docs), a.terms, a.terms_edited)
+print("Simple NOT with two terms")
 q3 = Query('heart NOT city', b.inverted_index_optimized, c.inverted_index_optimized, len(a.docs), a.terms, a.terms_edited)  # NOT query type one
+print("Simple NOT with one term")
 q4 = Query('NOT city', b.inverted_index_optimized, c.inverted_index_optimized, len(a.docs), a.terms, a.terms_edited)  # NOT query type two
+print("Proximity Query")
 q5 = Query('heart NEAR/3 city', b.inverted_index_optimized, c.inverted_index_optimized, len(a.docs), a.terms, a.terms_edited)
 # Complex Queries
 # Miss Spelled
+print("Miss Spelled Query")
 q6 = Query('hert AND city', b.inverted_index_optimized, c.inverted_index_optimized, len(a.docs), a.terms, a.terms_edited)
 # Wildcard Queries
+print("Wildcard Query")
 q7 = Query('heart AND c*ty', b.inverted_index_optimized, c.inverted_index_optimized, len(a.docs), a.terms, a.terms_edited)
 # Mixture of both
+print("Mixture of both")
 q8 = Query('hert AND c*ty', b.inverted_index_optimized, c.inverted_index_optimized, len(a.docs), a.terms, a.terms_edited)
 
